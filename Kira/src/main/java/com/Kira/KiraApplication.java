4109468c98ff342b74e46ee943a223801cdb4098
RuRu1229 <85883409+RuRu1229@users.noreply.github.com>
2023-10-08 21:42:33 +0800

    Add files via upload

---
+++ Kira/src/main/java/com/Kira/KiraApplication.java
@@ -0,0 +1,13 @@
+package com.Kira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiraApplication.class, args);
+	}
+
+}
---
+++ Kira/src/test/java/com/Kira/KiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
