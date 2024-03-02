26a21829292dbb1a8c790e9336c4d9db6063202b
DESKTOP-10LIQ3I\kiwi <kiwijang5473@gmail.com>
2024-03-03 07:44:02 +0800

    (feat) 新增 CORS 4200 設定、自動產生 db table 設定

--- Kira/src/main/java/com/Kira/KiraApplication.java
+++ Kira/src/main/java/com/Kira/KiraApplication.java
@@ -3,6 +3,9 @@ package com.Kira;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class KiraApplication {
@@ -11,4 +14,13 @@ public class KiraApplication {
 		SpringApplication.run(KiraApplication.class, args);
 	}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
